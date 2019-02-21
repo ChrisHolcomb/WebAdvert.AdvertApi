5054b2abf25a5ffe35ee274112e98cf769c543f9
Chris Holcomb <chris@pooldashboard.com>
2019-02-21 02:54:11 -0600

    Linking Amazon DynamoDB table and actions

---
+++ Services/IAdvertStorageService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+
+namespace WebAdvert.AdvertApi.Services
+{
+    public interface IAdvertStorageService
+    {
+         Task<string> Add(AdvertModel model);
+         Task Confirm(ConfirmAdvertModel model);
+    }
+}
